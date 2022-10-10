e799b10a9d3259e906016c48e0a7ee94784d90eb
DiamondDDF <ddfalerts@gmail.com>
2022-10-10 18:50:55 -0500

    0.0.5 test

--- greetings.go
+++ greetings.go
@@ -16,7 +16,7 @@ func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("Name was empty.")
 	}
-	message := fmt.Sprintf("🍊"+randomFormat()+"version 2", name)
+	message := fmt.Sprintf("🍊"+randomFormat()+"version 5", name)
 	return message, nil
 }
 
