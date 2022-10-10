a0d279b32701bccbf10fcea25d779d6579a08a21
DiamondDDF <ddfalerts@gmail.com>
2022-10-10 14:17:53 -0500

    initial commit

---
+++ greetings.go
@@ -0,0 +1,44 @@
+package greetings
+import ( 
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// run automatically at program startup
+func init(){
+	rand.Seed(time.Now().UnixNano())
+}
+
+func Hello(name string) (string, error) {
+	if name ==  "" {
+		return "", errors.New("Name was empty.")
+	}
+	message := fmt.Sprintf("🍊" + randomFormat(), name)
+	return message, nil
+}
+
+func Hellos( names []string) (map[string]string, error){
+	messages := make(map[string]string)
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		// In the map, associate the retrieved message with the name
+		messages[name] = message
+	}
+	return messages, nil
+}
+// randomFormat returns one of a set of greeting messages. The returned
+// message is selected at random.
+func randomFormat() string {
+	// A slice of message formats
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+	return formats[rand.Intn(len(formats))]
+}
